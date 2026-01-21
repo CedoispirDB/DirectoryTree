14dc65f1fed3b7980b08cb2c8bedc424534b2862
Marco Barreirinhas <marcodato03@gmail.com>
2026-01-20 21:17:42 -0300

    creates png image that looks more like a tree. Left debug outputs

--- tranverse.c
+++ tranverse.c
@@ -7,6 +7,7 @@
 #include "img_util.h"
 
 #define IMG_WIDTH 1024
+#define IMG_HEIGTH 1024
 
 typedef enum
 {
@@ -22,7 +23,19 @@ struct Node
     Node *sibling;
     int child_cnt;
     NODE_TYPE type;
+    int children_name_len;
 };
+
+typedef struct
+{
+    int parent_cnt;
+} TreeData;
+
+const char *basename(const char *path)
+{
+    const char *p = strrchr(path, '\\');
+    return p ? p + 1 : path;
+}
 
 Node *new_node(char *name, NODE_TYPE type)
 {
@@ -44,8 +57,8 @@ Node *new_node(char *name, NODE_TYPE type)
     new->child = NULL;
     new->sibling = NULL;
     new->child_cnt = 0;
+    new->children_name_len = 0;
     new->type = type;
-
     return new;
 }
 
@@ -57,11 +70,9 @@ void add_child(Node *parent, Node *child)
     if (!parent->child)
     {
         parent->child = child;
-        parent->child_cnt++;
     }
     else
     {
-        parent->child_cnt++;
         Node *cur = parent->child;
         while (cur->sibling)
         {
@@ -69,9 +80,12 @@ void add_child(Node *parent, Node *child)
         }
         cur->sibling = child;
     }
+
+    parent->child_cnt++;
+    parent->children_name_len += strlen(child->name);
 }
 
-void tranverse(const char *start_path, Node *root)
+void tranverse(const char *start_path, Node *root, TreeData *tree_data)
 {
     WIN32_FIND_DATA d;
     HANDLE h;
@@ -89,6 +103,12 @@ void tranverse(const char *start_path, Node *root)
 
     do
     {
+        // skip . files
+        if (d.cFileName[0] == '.')
+        {
+            continue;
+        }
+
         // Create new parent
         if (d.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
         {
@@ -96,11 +116,11 @@ void tranverse(const char *start_path, Node *root)
             if (strcmp(d.cFileName, ".") != 0 &&
                 strcmp(d.cFileName, "..") != 0)
             {
-
                 snprintf(next, sizeof(next), "%s\\%s", start_path, d.cFileName);
                 Node *new_root = new_node(d.cFileName, PARENT);
+                tree_data->parent_cnt++;
                 add_child(root, new_root);
-                tranverse(next, new_root);
+                tranverse(next, new_root, tree_data);
                 SetCurrentDirectory("..");
             }
         }
@@ -161,74 +181,131 @@ void to_uppercase(char *s)
 
 void create_tree(
     Node *n, unsigned char *img,
-    int x, int y)
+    int x, int y,
+    int max_needed_heigth,
+    int scale, int internal_padd, int gap, int arrow_length, int lvl)
 {
     if (!n)
     {
         return;
     }
 
-    int scale = 1;
-    int padd = 3;
-    int arrow_length = 20;
+    int next_level_expected_width = -1;
+    int middle = -1;
+    int start_children = -1;
 
     int title_size = (int)strlen(n->name);
     // -1 -> whitespace on the bitmap
-    int rw = BITMAP_SIZE * title_size * scale - 1 + padd * 2;
-    int rh = BITMAP_SIZE - 1 + padd * 2;
+    int rw = BITMAP_SIZE * title_size * scale - 1 + internal_padd * 2;
+    int rh = BITMAP_SIZE - 1 + internal_padd * 2;
 
     // int wx = x - rw / 2;
     to_uppercase(n->name);
 
     if (n->type == PARENT)
     {
+        // HANDLE ROOT
+        if (x == IMG_WIDTH / 2)
+        {
+            x = x - rw / 2;
+        }
         fill_rect(img, IMG_WIDTH, x, y, rw, rh, COLOR_RED);
-        draw_text_scale(img, IMG_WIDTH, x + padd, y + padd, n->name, scale);
+        draw_text_scale(img, IMG_WIDTH, x + internal_padd, y + internal_padd, n->name, scale);
         draw_arrow(img,
                    IMG_WIDTH, x + rw / 2, y + rh + 2, x + rw / 2, y + rh + arrow_length);
+
+        next_level_expected_width =
+            n->children_name_len * BITMAP_SIZE * scale +
+            ((internal_padd * 2) * n->child_cnt) - n->child_cnt + gap * (n->child_cnt - 1);
+        middle = x + rw / 2;
+        start_children = middle - next_level_expected_width / 2;
     }
     else
     {
         fill_rect(img, IMG_WIDTH, x, y, rw, rh, COLOR_YELLOW);
-        draw_text_scale(img, IMG_WIDTH, x + padd, y + padd, n->name, scale);
+        draw_text_scale(img, IMG_WIDTH, x + internal_padd, y + internal_padd, n->name, scale);
     }
 
-    create_tree(n->child, img, x, y + arrow_length + 20 + 20);
-    create_tree(n->sibling, img, x + title_size * BITMAP_SIZE + 50, y);
+    printf("(%i - %s): start_children at: %i, parent at: %i\n", lvl, n->name, start_children, x);
+
+    create_tree(
+        n->child, img,
+        start_children, y + arrow_length + 20 + 20,
+        max_needed_heigth,
+        scale, internal_padd, gap, arrow_length, lvl + 1);
+    
+    int next_sibling_pos = x + rw + internal_padd * 2 + gap;
+    create_tree(
+        n->sibling, img,
+        next_sibling_pos, y,
+        max_needed_heigth,
+        scale, internal_padd, gap, arrow_length, lvl);
+}
+
+void prepare_tree(Node *root, TreeData tree_data, unsigned char *img)
+{
+    if (img == NULL || root == NULL)
+    {
+        fprintf(stderr, "ERROR: NULL PARAMETERS");
+        return;
+    }
+
+    // white background
+    for (int i = 0; i < IMG_WIDTH * IMG_HEIGTH * 3; i++)
+    {
+        img[i] = 255;
+    }
+
+    int scale = 1;
+    int internal_padd = 3;
+    int arrow_length = 20;
+    int gap = 5;
+
+    int max_needed_height = tree_data.parent_cnt * BITMAP_SIZE * scale + ((internal_padd * 2) * tree_data.parent_cnt) - tree_data.parent_cnt + gap * (tree_data.parent_cnt - 1);
+    //  resize arrow acording to gap
+    create_tree(root, img, IMG_WIDTH / 2, 0, max_needed_height, scale, internal_padd, gap, arrow_length, 0);
 }
 
 int main()
 {
     const char *start_file = "C:\\Users\\marco\\Programming\\DirectoryTree";
 
-    Node *root = new_node("DirectoryTree", PARENT);
+    Node *root = NULL;
+    TreeData tree_data = {.parent_cnt = 0};
+    // save first parent
+    if (root == NULL)
+    {
+        char name[512];
+        const char *base = basename(start_file);
+        strcpy(name, base);
+        root = new_node(name, PARENT);
+        tree_data.parent_cnt++;
+    }
 
-    tranverse(start_file, root);
+    tranverse(start_file, root, &tree_data);
 
     printf("\n\nTREE\n\n");
+    printf("tree_data: %i\n", tree_data);
     walk(root, 0, false);
 
     int w = IMG_WIDTH;
-    int h = IMG_WIDTH;
-    unsigned char *img = malloc(w * h * 3);
+    int h = IMG_HEIGTH;
+
+    unsigned char *img = malloc(IMG_WIDTH * IMG_HEIGTH * 3);
     if (img == NULL)
     {
         fprintf(stderr, "ERROR: OUT OF MEMORY FOR IMG");
-        return 1;
     }
 
-    for (int i = 0; i < w * h * 3; i++)
-    {
-        img[i] = 255;
-    }
-
-    create_tree(root, img, 0, 0);
+    prepare_tree(root, tree_data, img);
 
     int ok = stbi_write_png("C:\\Users\\marco\\Programming\\DirectoryTree\\tree.png", w, h, 3, img, w * 3);
     if (!ok)
     {
         fprintf(stderr, "ERROR: FAILED TO WRITE PNG\n");
     }
+
+    free(img);
 
     return 0;
 }
